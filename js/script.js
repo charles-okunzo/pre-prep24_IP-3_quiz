81800ebce846bdf8ab2661631e9f47a25ad78487
charles-okunzo <okunzopeter01@gmail.com>
2022-02-05 21:07:22 +0300

    feature:dissapperaing content- added a button to enable questions to appear

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showMydiv()
+{
+    var showContent= document.getElementById("hiddendiv");
+    showContent.style.display="block"
+    var hideButton=document.getElementById("show");
+    hideButton.style.display="none"
+}
